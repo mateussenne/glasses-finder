0ad3b8e8f1c680c1c4d9ff6988a6d5b0cd341698
Mateus Senne <mateussenne@gmail.com>
2023-08-16 17:38:38 -0300

    Add aws upload and formidable package

--- src/pages/api/uploads/file-upload.ts
+++ src/pages/api/uploads/file-upload.ts
@@ -1,18 +1,40 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import formidable, { errors as formidableErrors } from "formidable";
 import type IncomingForm from "formidable/Formidable";
+import AWS from "aws-sdk";
+import fs from "fs";
 
+const s3 = new AWS.S3();
+const Bucket = process.env.AWS_S3_BUCKET_NAME;
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const form: IncomingForm = formidable();
-  let files;
+  return console.log(form);
   try {
-    [files] = await form.parse(req);
-    console.log(files);
+    const [files] = await form.parse(req);
+    const file = files[0];
+    if (!Bucket || file) {
+      return res.status(500).end();
+    }
+
+    return console.log(file);
+    await s3
+      .putObject({
+        Bucket,
+        Key: "hey12.png",
+        Body: fs.createReadStream(file),
+      })
+      .promise();
     return res.status(200).end();
   } catch (e) {
     console.log(e);
     return res.status(500).end();
   }
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default handler;
